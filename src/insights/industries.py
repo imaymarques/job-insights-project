a0db23ea2442483824136c6e43afa6a04bb63081
Mayara Marques <mayaramarquescosta@outlook.com>
2023-03-24 17:42:40 -0300

    feat: Implementing filter by industry function

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -14,18 +14,10 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
+    filter_by_industries = [
+        by_industry
+        for by_industry in jobs
+        if by_industry["industry"] == industry
+    ]
+    return filter_by_industries
     raise NotImplementedError
